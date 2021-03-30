b3e65c8e880b23fef697d9c56978731ba251f330
kazuyaseki <track613armory@gmail.com>
2021-03-30 18:11:28 +0900

    created class for canvas

--- src/pages/GraphicsProgrammingNyumon.tsx
+++ src/pages/GraphicsProgrammingNyumon.tsx
@@ -1,22 +1,39 @@
 import * as React from 'react'
 import styled from 'styled-components'
 
+class Canvas2DUtility {
+  private canvasElement: HTMLCanvasElement
+  public context2d: CanvasRenderingContext2D
+  constructor(canvas: HTMLCanvasElement) {
+    this.canvasElement = canvas
+    this.context2d = canvas.getContext('2d')! // 使うの自分だけなので non-null assertion
+  }
+
+  public drawRect(x: number, y: number, width: number, height: number, color: string) {
+    if (color) {
+      this.context2d.fillStyle = color
+    }
+    this.context2d.fillRect(x, y, width, height)
+  }
+}
+
 export default function GraphicsProgrammingNyumon() {
   const canvasRef = React.useRef<HTMLCanvasElement>(null)
 
   React.useEffect(() => {
     if (canvasRef.current) {
-      const context = canvasRef.current.getContext('2d')
+      const util = new Canvas2DUtility(canvasRef.current)
+      util.context2d
 
-      if (context) {
-        context.fillRect(0, 0, 100, 100)
+      if (util.context2d) {
+        util.drawRect(100, 100, 100, 200, '#343499')
 
-        context.font = 'bold 30px cursive'
-        context.fillStyle = '#ff00aa'
-        context.shadowBlur = 5
-        context.shadowOffsetY = 5
-        context.shadowColor = '#666666'
-        context.fillText('グラフィックプログラミンぐ', 100, 100)
+        util.context2d.font = 'bold 30px cursive'
+        util.context2d.fillStyle = '#ff00aa'
+        util.context2d.shadowBlur = 5
+        util.context2d.shadowOffsetY = 5
+        util.context2d.shadowColor = '#666666'
+        util.context2d.fillText('グラフィックプログラミンぐ', 100, 100)
       }
     }
   }, [])
@@ -28,4 +45,15 @@ export default function GraphicsProgrammingNyumon() {
   )
 }
 
-const Layout = styled.div``
+const Layout = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #555;
+  color: #f0f0f0;
+  text-align: center;
+  overflow: hidden;
+
+  > canvas {
+    margin: 0 auto;
+  }
+`
