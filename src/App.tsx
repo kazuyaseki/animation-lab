91f49c45bffd8f84923281cedaa4c66e84e8e5a9
kazuyaseki <track613armory@gmail.com>
2021-04-05 19:34:55 +0900

    add confetti

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Switch, Route, Link, useHistory } from 'react-router-dom'
 import CircleWave from './pages/CircleWave'
+import Confetti from './pages/Confetti'
 import FramerMotion from './pages/FramerMotion'
 import FramerMotionTransitioned from './pages/FramerMotionTransitioned'
 import FramerMotionTransitionFrom from './pages/FramerMotionTransitionedFrom'
@@ -10,53 +11,91 @@ import { AnimatedRoutes, RouteTransition } from './transition/RouteTransition'
 export default function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/graphics-programming-nyumon">graphics-programming-nyumon</Link>
-            </li>
-            <li>
-              <Link to="/circleWave">CircleWave</Link>
-            </li>
-            <li>
-              <Link to="/framer-motion">Framer Motion</Link>
-            </li>
-            <li>
-              <Link to="/framer-motion-from">Framer Motion From</Link>
-            </li>
-            <li>
-              <Link to="/framer-motion-transitioned">Framer Motion Transitioned</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path="/graphics-programming-nyumon">
-            <GraphicsProgrammingNyumon />
-          </Route>
-          <Route path="/circleWave">
-            <CircleWave />
-          </Route>
-          <Route path="/framer-motion">
-            <FramerMotion />
-          </Route>
-          <AnimatedRoutes exitBeforeEnter initial={false}>
-            <RouteTransition exact path="/framer-motion-from" slide={10}>
-              <FramerMotionTransitionFrom />
-            </RouteTransition>
-            <RouteTransition exact path="/framer-motion-transitioned" slide={10}>
-              <FramerMotionTransitioned />
-            </RouteTransition>
-          </AnimatedRoutes>
-          <Route path="/">
-            <GraphicsProgrammingNyumon />
-          </Route>
-        </Switch>
-      </div>
+      <Inside />
     </Router>
+  )
+}
+
+function Inside() {
+  const history = useHistory()
+  const [keys, setKeys] = useState<string[]>([])
+
+  useEffect(() => {
+    return history.listen((location, action) => {
+      console.log(location, action)
+      if (action === 'PUSH') {
+        setKeys((_keys) => [..._keys, location.pathname])
+      }
+      if (action === 'POP') {
+        setKeys((_keys) => {
+          const arr = [..._keys]
+          arr.pop()
+          return arr
+        })
+      }
+    })
+  }, [])
+
+  console.log(keys)
+
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/graphics-programming-nyumon">graphics-programming-nyumon</Link>
+          </li>
+          <li>
+            <Link to="/circleWave">CircleWave</Link>
+          </li>
+          <li>
+            <Link to="/confetti">Confetti</Link>
+          </li>
+          <li>
+            <Link to="/framer-motion">Framer Motion</Link>
+          </li>
+          <li>
+            <Link to="/framer-motion-from">Framer Motion From</Link>
+          </li>
+          <li>
+            <Link to="/framer-motion-transitioned">Framer Motion Transitioned</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/graphics-programming-nyumon">
+          <GraphicsProgrammingNyumon />
+        </Route>
+        <Route path="/circleWave">
+          <CircleWave />
+        </Route>
+        <Route path="/confetti">
+          <Confetti />
+        </Route>
+        <Route path="/framer-motion">
+          <FramerMotion />
+        </Route>
+        <AnimatedRoutes exitBeforeEnter initial={false}>
+          <RouteTransition exact path="/framer-motion-from">
+            <FramerMotionTransitionFrom />
+          </RouteTransition>
+          <RouteTransition
+            exact
+            path="/framer-motion-transitioned"
+            moveIn={keys[keys.length - 1] === '/framer-motion-transitioned'}
+            moveOut={keys[keys.length - 1] === '/framer-motion-transitioned'}
+          >
+            <FramerMotionTransitioned />
+          </RouteTransition>
+        </AnimatedRoutes>
+        <Route path="/">
+          <GraphicsProgrammingNyumon />
+        </Route>
+      </Switch>
+    </div>
   )
 }
